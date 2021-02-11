8a1030ffdd25b911067376f35dd617e7664a4096
Ruto <ruto@protonmail.ch>
2021-02-10 21:43:45 -0500

    Update DiscordService

--- DiscordStreamBot/Discord/DiscordService.cs
+++ DiscordStreamBot/Discord/DiscordService.cs
@@ -1,4 +1,8 @@
+using System.Reflection;
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace DiscordStreamBot.Discord
@@ -6,16 +10,36 @@ namespace DiscordStreamBot.Discord
     public class DiscordService : IDiscordService
     {
         private readonly ILogger _logger;
+        private readonly IConfiguration _config;
+        private readonly DiscordClient _discordClient;
 
-        public DiscordService(ILogger<DiscordService> logger)
+        public DiscordService(IConfiguration config, ILogger<DiscordService> logger)
         {
             _logger = logger;
+            _config = config;
+            _discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = config["Discord:Token"],
+                TokenType = TokenType.Bot
+            });
         }
 
-        public Task StartBot()
+        public async Task StartBot()
         {
             _logger.LogInformation("Discord | Starting Discord Bot");
-            return Task.CompletedTask;
+
+            await _discordClient.ConnectAsync();
+
+            var commands = _discordClient.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new[] { _config["Discord:Prefix"] }
+            });
+            commands.RegisterCommands(Assembly.GetExecutingAssembly());
+
+            foreach (var command in commands.RegisteredCommands)
+            {
+                _logger.LogInformation("Discord | Commands | Registed Command {command}", command.Key);
+            }
         }
     }
 }
